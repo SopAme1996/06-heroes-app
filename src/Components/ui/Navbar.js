85f6261fe30993c151ce8269e08ef2574c78d2f1
Luis Enrique Solis Puc <sop199642@gmail.com>
2021-07-23 18:53:35 -0500

    Se quito las variables que no se usan

--- src/Components/ui/Navbar.js
+++ src/Components/ui/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { startLogout } from '../../asincrono/authActionAsinc';
 import { useDispatch, useSelector } from "react-redux";
 
